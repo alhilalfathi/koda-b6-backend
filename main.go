2767324b3158936ef05f4aee2cfc5e200ae122e8
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-02 13:44:33 +0700

    feat: spliting models

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
+	"koda-b6-backend/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/matthewhartstonge/argon2"
 )
-
-type Response struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
-	Results any    `json:"results"`
-}
-
-type Users struct {
-	Id       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
 
 var argon = argon2.DefaultConfig()
 
@@ -37,9 +26,6 @@ func VerifyPassword(encodedHash, password string) bool {
 	}
 	return ok
 }
-
-var NextId = 1
-var UserList []Users
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -60,27 +46,27 @@ func main() {
 	r.Use(corsMiddleware())
 
 	r.POST("/register", func(ctx *gin.Context) {
-		var data Users
+		var data models.Users
 		err := ctx.ShouldBind(&data)
 
 		if err != nil {
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Register Failed",
 			})
 			return
 		}
 		if data.Email == "" || data.Password == "" {
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Email and Password cannot blank",
 			})
 			return
 		}
 
-		for i := 0; i < len(UserList); i++ {
-			if UserList[i].Email == data.Email {
-				ctx.JSON(http.StatusBadRequest, Response{
+		for i := 0; i < len(models.UserList); i++ {
+			if models.UserList[i].Email == data.Email {
+				ctx.JSON(http.StatusBadRequest, models.Response{
 					Success: false,
 					Message: "Email already exist",
 				})
@@ -90,7 +76,7 @@ func main() {
 
 		hashedPassword, err := HashPassword(data.Password)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, Response{
+			ctx.JSON(http.StatusInternalServerError, models.Response{
 				Success: false,
 				Message: "Failed to hash password",
 			})
@@ -98,50 +84,50 @@ func main() {
 		}
 
 		data.Password = hashedPassword
-		data.Id = NextId
-		UserList = append(UserList, data)
-		NextId++
-		ctx.JSON(http.StatusOK, Response{
+		data.Id = models.NextId
+		models.UserList = append(models.UserList, data)
+		models.NextId++
+		ctx.JSON(http.StatusOK, models.Response{
 			Success: true,
 			Message: "Register Success",
 		})
 	})
 
 	r.POST("/login", func(ctx *gin.Context) {
-		var data Users
+		var data models.Users
 		err := ctx.ShouldBind(&data)
 
 		if err != nil {
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Login Failed",
 			})
 			return
 		}
 		if data.Email == "" || data.Password == "" {
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Email and Password cannot blank",
 			})
 			return
 		}
 
-		for i := 0; i < len(UserList); i++ {
-			if UserList[i].Email == data.Email {
-				if VerifyPassword(UserList[i].Password, data.Password) {
-					ctx.JSON(http.StatusOK, Response{
+		for i := 0; i < len(models.UserList); i++ {
+			if models.UserList[i].Email == data.Email {
+				if VerifyPassword(models.UserList[i].Password, data.Password) {
+					ctx.JSON(http.StatusOK, models.Response{
 						Success: true,
 						Message: "Login successful",
 					})
 				} else {
-					ctx.JSON(http.StatusBadRequest, Response{
+					ctx.JSON(http.StatusBadRequest, models.Response{
 						Success: false,
 						Message: "Password Incorrect",
 					})
 				}
 				return
 			}
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Email Incorrect",
 			})
@@ -150,26 +136,26 @@ func main() {
 	})
 
 	r.GET("/users", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, Response{
+		ctx.JSON(http.StatusOK, models.Response{
 			Success: true,
 			Message: "List of users",
-			Results: UserList,
+			Results: models.UserList,
 		})
 	})
 
 	r.GET("/users/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
-		for i := range UserList {
-			if fmt.Sprint(UserList[i].Id) == id {
-				ctx.JSON(http.StatusOK, Response{
+		for i := range models.UserList {
+			if fmt.Sprint(models.UserList[i].Id) == id {
+				ctx.JSON(http.StatusOK, models.Response{
 					Success: true,
 					Message: fmt.Sprintf("Hello User %s", id),
-					Results: UserList[i],
+					Results: models.UserList[i],
 				})
 				return
 			}
 		}
-		ctx.JSON(404, Response{
+		ctx.JSON(404, models.Response{
 			Success: false,
 			Message: fmt.Sprintf("User %s not found", id),
 		})
@@ -177,45 +163,45 @@ func main() {
 
 	r.PATCH("/users/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
-		var newData Users
+		var newData models.Users
 		err := ctx.ShouldBind(&newData)
 
 		if err != nil {
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Input error",
 			})
 			return
 		}
-		for i := range UserList {
-			if fmt.Sprint(UserList[i].Id) == id {
+		for i := range models.UserList {
+			if fmt.Sprint(models.UserList[i].Id) == id {
 				if newData.Email != "" {
-					for j := range UserList {
-						if UserList[j].Email == newData.Email && UserList[j].Id != newData.Id {
-							ctx.JSON(http.StatusBadRequest, Response{
+					for j := range models.UserList {
+						if models.UserList[j].Email == newData.Email && models.UserList[j].Id != newData.Id {
+							ctx.JSON(http.StatusBadRequest, models.Response{
 								Success: false,
 								Message: "Email already registered",
 							})
 							return
 						}
 					}
-					UserList[i].Email = newData.Email
+					models.UserList[i].Email = newData.Email
 				}
 				if newData.Password != "" {
-					UserList[i].Password = newData.Password
+					models.UserList[i].Password = newData.Password
 				}
-				ctx.JSON(http.StatusOK, Response{
+				ctx.JSON(http.StatusOK, models.Response{
 					Success: true,
 					Message: "User data updated",
 				})
 				return
 			}
-			ctx.JSON(http.StatusBadRequest, Response{
+			ctx.JSON(http.StatusBadRequest, models.Response{
 				Success: false,
 				Message: "Input error",
 			})
 		}
-		ctx.JSON(http.StatusBadRequest, Response{
+		ctx.JSON(http.StatusBadRequest, models.Response{
 			Success: false,
 			Message: "User not found",
 		})
@@ -223,10 +209,10 @@ func main() {
 
 	r.DELETE("/users/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
-		for i := range UserList {
-			if fmt.Sprint(UserList[i].Id) == id {
-				UserList = append(UserList[:i], UserList[i+1:]...)
-				ctx.JSON(http.StatusOK, Response{
+		for i := range models.UserList {
+			if fmt.Sprint(models.UserList[i].Id) == id {
+				models.UserList = append(models.UserList[:i], models.UserList[i+1:]...)
+				ctx.JSON(http.StatusOK, models.Response{
 					Success: true,
 					Message: "User deleted",
 				})

bfdee7b46c808653fd6d5bf09dc63556974bc440
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-02 13:44:26 +0700

    feat: spliting models

---
+++ models/user_models.go
@@ -0,0 +1,16 @@
+package models
+
+type Response struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Results any    `json:"results"`
+}
+
+type Users struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var NextId = 1
+var UserList []Users

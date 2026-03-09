c1f3f62a01be963f55168b69c76b7df28976b273
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-09 10:51:07 +0700

    feat: add register endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"koda-b6-backend/internal/di"
-	"koda-b6-backend/internal/models"
 	"net/http"
 	"os"
 
@@ -49,116 +48,17 @@ func main() {
 
 	userHandler := container.UserHandler()
 
+	r.POST("/register", userHandler.Create)
+	// r.POST("/login", handlers.Login)
+
 	users := r.Group("/users")
 	{
-		// users.POST("/register", userHandler.Register)
-		// users.POST("/login", handlers.Login)
 		users.GET("/", userHandler.GetAll)
 		users.GET("/:id", userHandler.GetById)
+		users.GET("/:email", userHandler.GetByEmail)
 		users.PATCH("/:id", userHandler.Update)
 		users.DELETE("/:id", userHandler.Delete)
 	}
-
-	// r.GET("/users", func(ctx *gin.Context) {
-
-	// 	rows, err := conn.Query(context.Background(), `
-	// 	SELECT id, email, password
-	// 	FROM "USER"
-	// 	`)
-
-	// 	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Users])
-
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusBadRequest, models.Response{
-	// 			Success: false,
-	// 			Message: "Failed to get data users",
-	// 		})
-	// 		return
-	// 	}
-
-	// 	ctx.JSON(http.StatusOK, models.Response{
-	// 		Success: true,
-	// 		Message: "List of users",
-	// 		Results: users,
-	// 	})
-	// })
-
-	r.GET("/users/:id", func(ctx *gin.Context) {
-		id := ctx.Param("id")
-		for i := range models.UserList {
-			if fmt.Sprint(models.UserList[i].Id) == id {
-				ctx.JSON(http.StatusOK, models.Response{
-					Success: true,
-					Message: fmt.Sprintf("Hello User %s", id),
-					Results: models.UserList[i],
-				})
-				return
-			}
-		}
-		ctx.JSON(404, models.Response{
-			Success: false,
-			Message: fmt.Sprintf("User %s not found", id),
-		})
-	})
-
-	r.PATCH("/users/:id", func(ctx *gin.Context) {
-		id := ctx.Param("id")
-		var newData models.Users
-		err := ctx.ShouldBind(&newData)
-
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, models.Response{
-				Success: false,
-				Message: "Input error",
-			})
-			return
-		}
-		for i := range models.UserList {
-			if fmt.Sprint(models.UserList[i].Id) == id {
-				if newData.Email != "" {
-					for j := range models.UserList {
-						if models.UserList[j].Email == newData.Email && models.UserList[j].Id != newData.Id {
-							ctx.JSON(http.StatusBadRequest, models.Response{
-								Success: false,
-								Message: "Email already registered",
-							})
-							return
-						}
-					}
-					models.UserList[i].Email = newData.Email
-				}
-				if newData.Password != "" {
-					models.UserList[i].Password = newData.Password
-				}
-				ctx.JSON(http.StatusOK, models.Response{
-					Success: true,
-					Message: "User data updated",
-				})
-				return
-			}
-			ctx.JSON(http.StatusBadRequest, models.Response{
-				Success: false,
-				Message: "Input error",
-			})
-		}
-		ctx.JSON(http.StatusBadRequest, models.Response{
-			Success: false,
-			Message: "User not found",
-		})
-	})
-
-	r.DELETE("/users/:id", func(ctx *gin.Context) {
-		id := ctx.Param("id")
-		for i := range models.UserList {
-			if fmt.Sprint(models.UserList[i].Id) == id {
-				models.UserList = append(models.UserList[:i], models.UserList[i+1:]...)
-				ctx.JSON(http.StatusOK, models.Response{
-					Success: true,
-					Message: "User deleted",
-				})
-			}
-		}
-	})
 
 	r.Run(fmt.Sprintf("localhost:%s", os.Getenv("PORT")))
 }

0c553ed4058c3f56998e863048fc4339e4711c38
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-17 11:09:32 +0700

    chor: move cors origin url to .env

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,15 +2,18 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "http://localhost:8888")
+		godotenv.Load()
+		ctx.Header("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
 		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		ctx.Header("Access-Control-Allow-Headers", "Content-type")
+		ctx.Header("Access-Control-Allow-Headers", "Content-type, authorization")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.Data(http.StatusOK, "", []byte(""))
 		} else {

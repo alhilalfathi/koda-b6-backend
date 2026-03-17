8080344f4d5c4d07cbf6fac3fa104e04f639a171
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-17 13:13:25 +0700

    feat: add images to product models

--- internal/models/product_models.go
+++ internal/models/product_models.go
@@ -6,6 +6,7 @@ type Product struct {
 	Desc        string `json:"product_desc" db:"product_desc"`
 	Price       int    `json:"price" db:"price"`
 	Stock       int    `json:"stock" db:"stock"`
+	Images      string `json:"path" db:"path"`
 }
 type CreateProductRequest struct {
 	ProductName string `json:"product_name" db:"product_name"`

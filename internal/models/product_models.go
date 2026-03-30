5ce15370e370447006f2326ec189c5202b5e2a47
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-30 11:45:17 +0700

    feat: add category and images to models products

--- internal/models/product_models.go
+++ internal/models/product_models.go
@@ -6,6 +6,8 @@ type Product struct {
 	Desc        string `json:"product_desc" db:"product_desc"`
 	Price       int    `json:"price" db:"price"`
 	Stock       int    `json:"stock" db:"stock"`
+	Category    string `json:"category" db:"category"`
+	Images      string `json:"path" db:"path"`
 }
 type CreateProductRequest struct {
 	ProductName string `json:"product_name" db:"product_name"`

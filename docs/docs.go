68605577bc1f21fa9a5b0fdadd1092f2a178a265
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-13 14:41:00 +0700

    chore: adjust swagger router

--- docs/docs.go
+++ docs/docs.go
@@ -772,7 +772,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8888",
+	Host:             "https://hilal-backend.camps.fahrul.id",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Backend Coffeeshop",

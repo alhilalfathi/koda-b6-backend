49a4980eaf150875c01fca2b906f113d0e019a20
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-14 13:50:30 +0700

    chore: change OrderDate type data

--- internal/models/transaction_models.go
+++ internal/models/transaction_models.go
@@ -1,18 +1,20 @@
 package models
 
+import "time"
+
 type Transaction struct {
-	Id          int    `json:"transaction_id" db:"id"`
-	TrxId       string `json:"trx_id" db:"trx_id"`
-	UserId      int    `json:"user_id" db:"user_id"`
-	OrderDate   string `json:"order_date" db:"order_date"`
-	Fullname    string `json:"fullname" db:"fullname"`
-	Email       string `json:"email" db:"email"`
-	Address     string `json:"address" db:"address"`
-	Delivery    string `json:"delivery" db:"delivery"`
-	DeliveryFee int    `json:"delivery_fee" db:"delivery_fee"`
-	Tax         int    `json:"tax" db:"tax"`
-	Total       int    `json:"total" db:"total"`
-	OrderStatus string `json:"status_order" db:"status_order"`
+	Id          int       `json:"transaction_id" db:"id"`
+	TrxId       string    `json:"trx_id" db:"trx_id"`
+	UserId      int       `json:"user_id" db:"user_id"`
+	OrderDate   time.Time `json:"order_date" db:"order_date"`
+	Fullname    string    `json:"fullname" db:"fullname"`
+	Email       string    `json:"email" db:"email"`
+	Address     string    `json:"address" db:"address"`
+	Delivery    string    `json:"delivery" db:"delivery"`
+	DeliveryFee int       `json:"delivery_fee" db:"delivery_fee"`
+	Tax         int       `json:"tax" db:"tax"`
+	Total       int       `json:"total" db:"total"`
+	OrderStatus string    `json:"status_order" db:"status_order"`
 }
 
 type CreateTransactionRequest struct {
@@ -52,7 +54,7 @@ type TransactionItem struct {
 type TransactionDetailResponse struct {
 	Id          int               `json:"id"`
 	TrxId       string            `json:"trx_id"`
-	OrderDate   string            `json:"order_date"`
+	OrderDate   time.Time         `json:"order_date"`
 	Total       int               `json:"total"`
 	StatusOrder string            `json:"status_order"`
 	Items       []TransactionItem `json:"items"`
